f7383ccc08ff5df2bf13040b3ae72da3f96e1382
Muhammad Musah <mmusah1@gmail.com>
2019-04-04 18:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var greeting = "Hello, everybody";
 
-var specialGuest = "Hello, Neil deGrasse Tyson";
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "Let's talk about space.";
-
-var conversation = `${topic}`;
